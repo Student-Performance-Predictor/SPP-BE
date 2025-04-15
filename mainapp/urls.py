85c3b8e6342f55c7ef018ab61448ced3789852de
vatsala824 <vatsalaverma824@gmail.com>
2025-04-15 22:05:19 +0530

    schools apis updation with 2 new and corrected the response at error

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from rest_framework_simplejwt.views import TokenRefreshView
 from .views import login, validate_token
-from .views import add_school, view_school, update_school, delete_school
+from .views import get_all_schools, get_school_names_with_id, add_school, view_school, update_school, delete_school
 from .views import add_principal, view_principal, update_principal, delete_principal
 
 urlpatterns = [
@@ -11,6 +11,8 @@ urlpatterns = [
     path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
 
     # CRUD APIs of schools
+    path('getAllSchools/', get_all_schools, name='get_all_schools'),
+    path('getSchoolsNames/', get_school_names_with_id, name='get_school_names_with_id'),
     path('addSchool/', add_school, name='add_school'),
     path('viewSchool/<int:pk>/', view_school, name='view_school'),
     path('updateSchool/<int:pk>/', update_school, name='update_school'),
