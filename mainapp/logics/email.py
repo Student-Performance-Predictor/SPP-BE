3553d6d8f68d688fd808f724b32b2f57d3227b1c
atharvarajsinha <sinhaatharva@gmail.com>
2025-12-12 11:38:52 +0530

    added async mailing system

--- mainapp/logics/email.py
+++ mainapp/logics/email.py
@@ -1,8 +1,8 @@
 from django.core.mail import EmailMultiAlternatives
 from django.template.loader import render_to_string
 from django.utils.html import strip_tags
+import threading
 
-# Email Sender
 def send_email_sync(subject, template_name, context, recipient_email):
     try:
         html_content = render_to_string(template_name, context)
@@ -18,5 +18,12 @@ def send_email_sync(subject, template_name, context, recipient_email):
         email.send()
         
     except Exception as e:
-        error_message = f"Email sending failed for {recipient_email}. Error: {e}"
-        print(error_message)
+        print(f"Email sending failed for {recipient_email}. Error: {e}")
+
+def send_email_background(subject, template_name, context, recipient_email):
+    thread = threading.Thread(
+        target=send_email_sync,
+        args=(subject, template_name, context, recipient_email)
+    )
+    thread.daemon = True
+    thread.start()
